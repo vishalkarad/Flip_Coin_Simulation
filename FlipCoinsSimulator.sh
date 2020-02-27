aa6893f28dff6d38e2fa7b0751d002ffdeef6b30
Your Name <you@example.com>
2020-02-27 16:35:54 +0530

    Added : Flipping Coin Store The Singlet Combination In Dictionary And Find Percentage

--- FlipCoinsSimulator.sh
+++ FlipCoinsSimulator.sh
@@ -3,10 +3,33 @@
 HEAD=0
 TAIL=1
 
+# variables
+declare -A dictionary
+count=0
+headPercentage=0
+tailPercentage=0
 random=$((RANDOM%2))
-if [ $random -eq $HEAD ]
+
+if [ $random -eq HEAD ]
 then
-   echo "Head"
+   printf "Head"
 else
-   echo "Tail"
+	printf "Tail"
 fi
+
+# Find percentage of the singlet combination
+for (( index=1; index<=10; index++ ))
+do
+	random1=$(( RANDOM%2 ))
+	if [ $random1 -eq $HEAD ]
+   then
+   	dictionary[$index]=Head
+		(( ++count ))
+	else
+   	dictionary[$index]=Tail
+	fi
+
+done
+headPercentage=`echo "$count/10*100" | bc -l`
+tailPercentag=`echo "$((10-$count))/10*100 " | bc -l`
+
